db64e5a7cc5e7944b001445196fd91fb3692fd07
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-06-05 14:54:25 -0300

    home page added

---
+++ src/screen/ChatPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const ChatPage = () => {
+    return(
+        <div>
+            <p>Teste XXXXXX</p>
+            
+        </div>
+        
+    )
+    
+}
+export default ChatPage
