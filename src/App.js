3151df2d187c6e41240c2e7ff9836ece578c2e7c
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-06-05 14:54:46 -0300

    new installations to route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Enterpage from './screen/Enterpage.js';
+import ChatPage from './screen/ChatPage.js';
 import Footer from './components/Footer.js';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-    
-  
-      <Enterpage/>
+    <Router>
+      <div>
       
-      <Footer/>
-    </div>
+        <Route path='/'>
+          <Enterpage/>
+        </Route>
+      
+        
+        <Footer/>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Enterpage/>}/>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/' element={<ChatPage/>}/>
+      </Routes>
+    </Router>
     
   );
 }
