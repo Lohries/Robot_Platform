b67f4018a629db427d39e0656de56b0b62f6553b
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-06-07 00:46:56 -0300

    aboutus screen and rout

---
+++ src/screen/AboutUs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const AboutUs = () => {
+    return(
+        <div>
+            <p>Teste XXXXXX</p>
+            
+        </div>
+        
+    )
+    
+}
+export default AboutUs
--- src/screen/Enterpage.js
+++ src/screen/Enterpage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Button_Enter from "../components/Button_Enter";
 import '../styles/Enter_page_styles.css'
-
+import {Outlet} from 'react-router-dom';
+import Footer from "../components/Footer";
 
 
 const Enterpage = () => {
     return(
         <div>
-            <Button_Enter/>
+            <Button_Enter />
             
+            <Footer />
         </div>
         
     )
