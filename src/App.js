07b226420cef0457f612039b5cacdbd029b60860
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-06-09 22:44:14 -0300

    router install

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ChatPage from './screen/ChatPage.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './screen/Home.js';
 import AboutUs from './screen/AboutUs.js';
+import Classifier from './screen/Classifier.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Enterpage />}/>
           <Route path="/home" element={<Home />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/classifier" element={<Classifier />} />
           <Route path="/aboutUs" element={<AboutUs />} />
       </Routes>
     </Router>
