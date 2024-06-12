44b88fe32c0bd802f2349457be9644591984e97c
Guilherme Marques de Lima <136756907+GuiMLima@users.noreply.github.com>
2024-06-12 17:34:09 -0300

    New buttons have been added on the Classifier page

---
+++ src/components/ActivateEyes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const ActivateInmoovEyes = () => {
+  return (
+    <div className="activate-button">
+        <button>Activate Inmoov eyes</button>
+    </div>
+  )  
+} 
+
+export default ActivateInmoovEyes
---
+++ src/components/InsertImage_Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const InsertImageButton = () => {
+    return(
+        <div className="insert-button">
+            <button>Insert image</button>
+        </div>
+    )
+}
+
+export default InsertImageButton
